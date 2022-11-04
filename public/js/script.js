36c484b6610692ab9096f96958e5c9c8f86ccb69
C. W. Chan <cwchan0212@gmail.com>
2022-11-04 21:30:22 +0000

    completed

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,14 @@
-
-const base = "http://localhost:5000";
+const base = "https://self-passport-google.cwchan0212.repl.co/";
 
 function login() {
-    window.open("http://localhost:5000/auth/google", "_self");
+    window.open(base + "/auth/google", "_self");
 }
 
 function logout() {
-    window.open("http://localhost:5000/logout", "_self");
+    window.open(base + "/logout", "_self");
 }
 
-
 function changeIt(par) {
-    // par = par ? 1 : 0;
     let uri = window.location.href;
-    // alert(uri)
     window.open(uri, "_self");
 }
